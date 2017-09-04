94ffbfe85b78e510f3cb967e2d0bb75c2d48a6cc
Pratik Sampat <psampat@vailsys.com>
2017-09-04 15:11:31 -0500

    wrote schema for post, created populatedb script, and initial stages of first route in server.js

--- src/populate.js
+++ src/populate.js
@@ -1,6 +1,14 @@
+const Post = require('./post');
+const mongoose = require('mongoose');
 const fs = require('fs');
 
 let savedPosts = null;
+
+mongoose.Promise = global.Promise;
+mongoose.connect(
+  'mongodb://localhost/so-posts',
+  { useMongoClient: true }
+);
 
 const readPosts = () => {
   // cache posts after reading them once
@@ -12,7 +20,21 @@ const readPosts = () => {
 };
 
 const populatePosts = () => {
-  // TODO: implement this
+  readPosts();
+  const promises = savedPosts.map((p) => {
+    return new Post(p).save();
+  });
+  return Promise.all(promises)
+    .then((result) => {
+      console.log('done', result);
+      mongoose.disconnect();
+      return result;
+    })
+    .catch((err) => {
+      console.log('ERROR', err);
+      throw new Error(err);
+    });
 };
 
+populatePosts();
 module.exports = { readPosts, populatePosts };
--- src/server.js
+++ src/server.js
@@ -1,12 +1,22 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const Post = require('./post');
 
 const STATUS_USER_ERROR = 422;
-
+const port = process.env.PORT || 3000;
 const server = express();
 // to enable parsing of json bodies for post requests
 server.use(bodyParser.json());
 
-// TODO: write your route handlers here
+server.get('/accepted-answer/:soID', (req, res) => {
+  const { soID } = req.params;
+  Post.find({ soID }, (err, data) => {
+    if (err) {
+      return res.json({ err });
+    }
+    return res.json({ data });
+  });
+});
 
+server.listen(port);
 module.exports = { server };
